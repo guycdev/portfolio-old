b559953fcbde0125c553fc643cfa01d200d3dbf0
gcWDev <cohenguy74@gmail.com>
2023-09-07 10:22:41 -0400

    added new svg masks

--- src/utilities/overlayArr.js
+++ src/utilities/overlayArr.js
@@ -1,7 +1,10 @@
 import brush from "../assets/overlays/brush.svg";
 import globe from "../assets/overlays/globe.svg";
 import grid from "../assets/overlays/grid.svg";
+import mysql from "../assets/overlays/mysql.svg";
+import git from "../assets/overlays/git.svg";
+import css from "../assets/overlays/css.svg";
 
-const overlays = [brush, globe, grid];
+const overlays = [brush, globe, grid, mysql, git, css];
 
 export default overlays;
