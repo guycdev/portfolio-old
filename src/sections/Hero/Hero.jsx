a4347b7bfce74041321a95a28f0f9c249b5f6b0f
gcWDev <cohenguy74@gmail.com>
2023-09-07 10:56:43 -0400

    Added transitions to animation changes

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeroCta from "./HeroCta";
 import WordAnimation from "./WordAnimation";
 import styles from "./Hero.module.css";
+import overlays from "../../utilities/overlayArr.js";
 
 export default function Hero() {
+  const [overlayIndex, setOverlayIndex] = useState(0);
+  const [randomLetters, setRandomLetters] = useState(letterRandomizer());
+
+  useEffect(() => {
+    window.addEventListener("mousemove", () =>
+      setRandomLetters(letterRandomizer)
+    );
+
+    const interval = setInterval(() => {
+      setOverlayIndex((prev) => (prev + 1) % overlays.length);
+    }, 3000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  function letterRandomizer() {
+    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    const upperCase = lowerCase.toUpperCase();
+
+    let randomizer = "";
+
+    for (let i = 0; i < 1500; i++) {
+      randomizer += lowerCase[randomNumber(24)];
+      randomizer += upperCase[randomNumber(24)];
+    }
+
+    return randomizer;
+  }
+
+  function randomNumber(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  const wordAnimationArr = overlays.map((overlay, index) => {
+    return (
+      <WordAnimation
+        overlayIndex={overlayIndex}
+        id={index}
+        key={index}
+        overlay={overlay}
+        randomLetters={randomLetters}
+      />
+    );
+  });
+
   return (
     <section className={styles.container}>
       <HeroCta />
-      <WordAnimation />
+      {wordAnimationArr}
     </section>
   );
 }
--- src/sections/Hero/WordAnimation.jsx
+++ src/sections/Hero/WordAnimation.jsx
@@ -1,66 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import React from "react";
 import styles from "./Hero.module.css";
-import overlays from "../../utilities/overlayArr.js";
 
-export default function WordAnimation() {
-  const [randomLetters, setRandomLetters] = useState(letterRandomizer());
-  const [overlayIndex, setOverlayIndex] = useState(0);
-
-  function letterRandomizer() {
-    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    const upperCase = lowerCase.toUpperCase();
-
-    let randomizer = "";
-
-    for (let i = 0; i < 1500; i++) {
-      randomizer += lowerCase[randomNumber(24)];
-      randomizer += upperCase[randomNumber(24)];
-    }
-
-    return randomizer;
-  }
-
-  function randomNumber(max) {
-    return Math.floor(Math.random() * max);
-  }
-
-  useEffect(() => {
-    window.addEventListener("mousemove", () =>
-      setRandomLetters(letterRandomizer)
-    );
-
-    const interval = setInterval(() => {
-      setOverlayIndex((prev) => (prev + 1) % overlays.length);
-    }, 5000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+export default function WordAnimation(props) {
+  const { overlayIndex, id, overlay, randomLetters } = props;
 
   const maskStyle = {
-    maskImage: `url(${overlays[overlayIndex]})`,
-    WebkitMaskImage: `url(${overlays[overlayIndex]})`,
+    maskImage: `url(${overlay})`,
+    WebkitMaskImage: `url(${overlay})`,
   };
 
+  const isVisible = overlayIndex == id;
+
   return (
-    <motion.div
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 0.6,
-        transition: {
-          duration: 1.3,
-          delay: 1,
-          type: "spring",
-        },
-      }}
+    <div
       className={styles.wordContainer}
+      style={{
+        opacity: isVisible ? 1 : 0,
+        height: isVisible ? "400px" : "0px",
+      }}
     >
       <div className={styles.maskContainer} style={maskStyle}>
         <p>{randomLetters}</p>
       </div>
-    </motion.div>
+    </div>
   );
 }
