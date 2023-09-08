3b06902c56445982ee2c92b9c968ab446561e91f
gcWDev <cohenguy74@gmail.com>
2023-09-08 13:58:58 -0400

    added code snippet funcitonaity

--- src/sections/AboutMe/AboutMe.jsx
+++ src/sections/AboutMe/AboutMe.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./AboutMe.module.css";
 import ProfileCard from "./ProfileCard";
 import CodeSnippet from "./CodeSnippet";
@@ -12,9 +12,103 @@ export default function AboutMe() {
 
   const featuresArr = [
     {
-      name: "Feature 1",
-      description: "This is feature 1",
-      code: `<div className="example">\n  <p>Hello, World!</p>\n</div>`,
+      name: "Minions Cursor Follower",
+      description:
+        "A minion face whose eyes follow mouse positioning using trigonometry",
+      code: `export default function MinionFollower() {
+  const [pupilStyle, setPupilStyle] = 
+    useState({
+    left: "50%",
+    top: "50%",
+    transform: "rotate(0deg)",
+  });
+
+  const eyeRadius = 8.96;
+
+  const eyeRefs = [useRef(null), useRef(null)];
+
+  function anglePositining(event) {
+    eyeRefs.forEach((eyeRef) => {
+      const eye = 
+      eyeRef.
+      current.
+      getBoundingClientRect();
+      const eyeCenterX = eye.left 
+      + eye.width 
+      / 2;
+      const eyeCenterY = eye.top 
+      + eye.height 
+      / 2;
+      const angle = Math.atan2(
+        event.clientY - eyeCenterY,
+        event.clientX - eyeCenterX
+      );
+
+      const pupilX = 
+      eyeRadius 
+      * Math.cos(angle) + 2.25 
+      * eyeRadius;
+      const pupilY =
+      eyeRadius 
+      * Math.sin(angle) 
+      + 2.2 
+      * eyeRadius;
+
+      const rotation = angle * (180 / Math.PI);
+
+      setPupilStyle({
+        left: pupilX + "px",
+        top: pupilY + "px",
+        transform: \`rotate(\${rotation}deg)\`,
+      });
+    });
+  }
+
+  useEffect(() => {
+    window.addEventListener("mousemove", 
+    anglePositining);
+
+    return () => {
+      window.removeEventListener("mousemove", 
+      anglePositining);
+    };
+  }, []);
+
+  return (
+      <div className="card">
+        <div className={styles.goggleContainer}>
+          <div className={styles.strapContainer}>
+            <div className={styles.eyeWrapper}>
+              <div 
+              className={styles.eye} 
+              ref={eyeRefs[0]}
+              >
+                <div className={styles.eyeBalls}>
+                  <div className={styles.pupels} 
+                  style={pupilStyle}
+                  >
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.eyeWrapper}>
+              <div 
+              className={styles.eye} 
+              ref={eyeRefs[1]}
+              >
+                <div className={styles.eyeBalls}>
+                  <div className={styles.pupels} 
+                  style={pupilStyle}
+                  >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    }
+  }`,
     },
     {
       name: "Feature 2",
--- src/sections/AboutMe/CodeSnippet.jsx
+++ src/sections/AboutMe/CodeSnippet.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { synthwave84 } from "react-syntax-highlighter/dist/esm/styles/prism";
+import styles from "./AboutMe.module.css";
+import { motion } from "framer-motion";
 
-export default function CodeSnippet() {
-  return <div className="card"></div>;
+export default function CodeSnippet(props) {
+  const { feature } = props;
+
+  return (
+    <div className={`card ${styles.codeSnippetContainer}`}>
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+          transition: {
+            delay: 0.4,
+          },
+        }}
+      >
+        <SyntaxHighlighter language="javascript" style={synthwave84}>
+          {feature.code}
+        </SyntaxHighlighter>
+      </motion.div>
+    </div>
+  );
 }
