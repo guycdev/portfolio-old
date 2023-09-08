6fc74b4df6fcbe18260ee5424d90fdf080274185
gcWDev <cohenguy74@gmail.com>
2023-09-08 13:23:52 -0400

    added feature controller

--- src/sections/AboutMe/AboutMe.jsx
+++ src/sections/AboutMe/AboutMe.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./AboutMe.module.css";
 import ProfileCard from "./ProfileCard";
 import CodeSnippet from "./CodeSnippet";
+import AboutMeContent from "./AboutMeContent";
+import AboutMeFeatures from "./AboutMeFeatures";
 
 export default function AboutMe() {
+  const [feature, setFeature] = useState(0);
+
+  console.log(feature);
+
+  const featuresArr = [
+    {
+      name: "Feature 1",
+      description: "This is feature 1",
+      code: `<div className="example">\n  <p>Hello, World!</p>\n</div>`,
+    },
+    {
+      name: "Feature 2",
+      description: "This is feature 2",
+      code: `console.log("Hello, World!");`,
+    },
+  ];
+
   return (
     <section className={styles.container}>
       <ProfileCard />
       <div className={styles.miscContainer}>
-        <div className="card"></div>
-        <div className="card"></div>
+        <AboutMeContent />
+        <AboutMeFeatures
+          features={featuresArr}
+          setFeature={setFeature}
+          activeFeature={feature}
+        />
       </div>
-      <CodeSnippet />
+      <CodeSnippet feature={featuresArr[feature]} />
     </section>
   );
 }
--- src/sections/AboutMe/ProfileCard.jsx
+++ src/sections/AboutMe/ProfileCard.jsx
@@ -1,16 +1,59 @@
 import React from "react";
 import styles from "./AboutMe.module.css";
 import Heading from "../../components/Heading";
+import Button from "../../components/Button";
+import blob from "../../assets/blob/blob.png";
+import { motion } from "framer-motion";
 
 export default function ProfileCard() {
+  const breathingVariant = {
+    initial: {
+      x: -500,
+      scale: 0.7,
+    },
+    breathe: {
+      rotate: [60, -60, 60],
+      scale: [0.7, 0.9, 0.7],
+      x: [0, 0, 0],
+      transition: {
+        repeat: Infinity,
+        ease: "easeInOut",
+        duration: 4,
+      },
+    },
+  };
+
   return (
-    <div className="card">
+    <div className={`card ${styles.aboutMeProfileContainer}`}>
+      <motion.div
+        className={styles.blobContainer}
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+          transition: {
+            delay: 0.5,
+            duration: 1,
+          },
+        }}
+      >
+        <motion.img
+          src={blob}
+          alt="blobs"
+          className={styles.blob}
+          initial="initial"
+          animate="breathe"
+          variants={breathingVariant}
+        />
+      </motion.div>
       <h3>
         <Heading content="Guy Cohen" />
       </h3>
       <h5>
         <Heading content="cohenguy74@gmail.com" />
       </h5>
+      <Button style="primary-btn" text="Download CV" />
     </div>
   );
 }
