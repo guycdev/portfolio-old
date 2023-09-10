819f7f927ea4f43a89cd73db39d16eeb3c552bbf
gcWDev <cohenguy74@gmail.com>
2023-09-10 14:33:47 -0400

    added form

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./Contact.module.css";
 import AnimatedBlocks from "./AnimatedBlocks";
+import ContactForm from "./ContactForm";
 
 export default function Contact() {
   const random = Math.floor(Math.random() * 100);
 
   return (
     <section className={styles.contactContainer}>
-      <div></div>
+      <ContactForm />
       <AnimatedBlocks amount={random} />
     </section>
   );
