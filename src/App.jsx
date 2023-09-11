255c4249e9c27648ec02d023c8f3e09082803578
gcWDev <cohenguy74@gmail.com>
2023-09-11 10:08:44 -0400

    added some basic controls to resume page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import Hero from "./sections/Hero/Hero";
 import AboutMe from "./sections/AboutMe/AboutMe";
 import Projects from "./sections/Projects/Projects";
 import Contact from "./sections/Contact/Contact";
+import Resume from "./sections/Resume/Resume";
+import Profile from "./sections/Resume/Profile";
 
 const route = createBrowserRouter(
   createRoutesFromElements(
@@ -17,6 +19,11 @@ const route = createBrowserRouter(
       <Route index element={<Hero />} />
       <Route path="/about-me" element={<AboutMe />} />
       <Route path="/projects" element={<Projects />} />
+      <Route path="/resume" element={<Resume />}>
+        <Route index element={<Profile />} />
+        <Route path="education" element={<h2>hi</h2>} />
+        <Route path="involvment" element={<h3>hello</h3>} />
+      </Route>
       <Route path="/get-in-touch" element={<Contact />} />
     </Route>
   )
