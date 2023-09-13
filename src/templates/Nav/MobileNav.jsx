fe6c4e76718c7b8779ad7bdc35aeed854ff92255
gcWDev <cohenguy74@gmail.com>
2023-09-13 16:51:18 -0400

    maybe new fix

--- src/templates/Nav/MobileNav.jsx
+++ src/templates/Nav/MobileNav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ReactSVG } from "react-svg";
 import close from "../../assets/close.svg";
 import { motion } from "framer-motion";
