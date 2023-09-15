97e580116a83c7d79627671616c83639398d346e
gcWDev <cohenguy74@gmail.com>
2023-09-15 11:32:31 -0400

    added cool mouse effect

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import HeroCta from "./HeroCta";
 import styles from "./Hero.module.css";
 import overlays from "../../utilities/overlayArr.js";
@@ -6,8 +6,32 @@ import overlays from "../../utilities/overlayArr.js";
 import WordAnimationParent from "./WordAnimationParent";
 
 export default function Hero() {
+  const blobRef = useRef(null);
+
+  useEffect(() => {
+    const handlePointerMove = (event) => {
+      const { clientX, clientY } = event;
+
+      blobRef.current.animate(
+        {
+          left: `${clientX}px`,
+          top: `${clientY}px`,
+        },
+        { duration: 3000, fill: "forwards" }
+      );
+    };
+
+    window.addEventListener("pointermove", handlePointerMove);
+
+    return () => {
+      window.removeEventListener("pointermove", handlePointerMove);
+    };
+  }, []);
+
   return (
     <section className={styles.container}>
+      <div className={styles.blob} ref={blobRef}></div>
+      <div className={styles.blur}></div>
       <HeroCta />
       <div className={styles.wordAnimationContainer}>
         <WordAnimationParent overlays={overlays} />
