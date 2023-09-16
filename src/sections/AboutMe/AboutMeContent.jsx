6ce9502155517308fab97c67e01baa50b6575d09
gcWDev <cohenguy74@gmail.com>
2023-09-16 12:03:35 -0400

    added responsivness for new about me section

--- src/sections/AboutMe/AboutMeContent.jsx
+++ src/sections/AboutMe/AboutMeContent.jsx
@@ -17,10 +17,15 @@ export default function AboutMeContent() {
         </p>
       </div>
       <p>
-        <Heading content="I'm Guy, an entrepenurial web developer always looking to undertake new ventures and grow my skill set. From bootstrapping a business to over $350K in lifetime revenue, to currently running a web agency tailored to mortgage broakers, I always love to learn new skills and turn ideas to solutions. I'm currently completing a dual degree in Computer Science and Finance and am looking to provide that big picture view on your development team." />
-
-        {/* <Heading content="Currently finishing dual degrees in Computer Science and Finance while also utilizing the power of code to generate $362K in lifetime revenue, I'm dedicated to pushing the boundaries of my knowledge and filling any gaps in my skill set. I love working with React and Node and whenever I'm not trying to add cool visual components to my projects you'll find me playing basketball or at the gym." /> */}
+        <Heading content="I'm Guy, an entrepenurial web developer always looking to undertake new ventures and grow my skill set. Between 2020 - 2022 I ran an automation business that generated $350K in lifetime revenue, and currently I run a web agency tailored towards mortgage broakers." />
+      </p>
+      <p>
+        <Heading content="In addition to my hands-on experience, I'm actively pursuing dual degrees in Computer Science and Finance. The two domains synergise to give me the growth mindset and big picture view required to succeed on your development team." />
       </p>
     </div>
   );
+}
+
+{
+  /* <Heading content="Currently finishing dual degrees in Computer Science and Finance while also utilizing the power of code to generate $362K in lifetime revenue, I'm dedicated to pushing the boundaries of my knowledge and filling any gaps in my skill set. I love working with React and Node and whenever I'm not trying to add cool visual components to my projects you'll find me playing basketball or at the gym." /> */
 }
