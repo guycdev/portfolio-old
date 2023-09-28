2106730c785a3da0118153411ae1f618903901e1
gcWDev <cohenguy74@gmail.com>
2023-09-27 21:49:44 -0400

    fixed some bullet points

--- src/sections/Resume/Experience.jsx
+++ src/sections/Resume/Experience.jsx
@@ -14,7 +14,7 @@ export default function Experiences() {
         "Engineered a CRUD Node API, reducing communication time by 80%.",
         "Designed modular UI for easy theme swaps for future projects.",
         "Learned about db security, and responsive design.",
-        "Earned $12K from 12 projects.",
+        "Managed 14 projects to date.",
       ],
     },
     {
@@ -25,10 +25,10 @@ export default function Experiences() {
       geo: "Toronto, ON, Canada",
       bullets: [
         "Utilized Python and Selenium for e-commerce automation.",
-        "Crafted Excel dashboard for $40K real-time inventory tracking.",
+        "Crafted Excel dashboard for real-time inventory tracking.",
         "Boosted brand via diverse social media channels.",
         "Established long-term relationships with wholesale and D2C clients",
-        "Hit $350K lifetime revenue.",
+        "Led the business to profitability",
       ],
     },
   ];
