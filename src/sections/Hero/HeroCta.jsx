115335cd30094f5726747c2ce663ca1ab3cc0847
gcWDev <cohenguy74@gmail.com>
2023-12-05 11:58:52 -0500

    changed cta

--- src/sections/Hero/HeroCta.jsx
+++ src/sections/Hero/HeroCta.jsx
@@ -37,7 +37,7 @@ export default function HeroCta() {
       </motion.h3>
       <p className="subheading">
         {/* <Heading content="I'm Guy, an aspiring innovator in both code and commerce, ready to contribute a big-picture skillset to your team. With a track record of turning ideas into profit through entrepreneurial experience, I bring the growth mindset required to succeed." /> */}
-        <Heading content="From bootstrapping 2 businesses to honing dual degrees in CS and Finance, I bring a unique skill set to the table. Ready to contribute this diverse perspective to your team." />
+        <Heading content="From bootstrapping two businesses to holding dual degrees in CS and Finance, I bring a unique skill set to the table. Ready to contribute this diverse perspective to your team." />
       </p>
       <div className={styles.btnContainer}>
         <Link to="get-in-touch">
